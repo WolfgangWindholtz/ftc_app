cefee749035ea2a377fbf69f49b7936c20603d3a
Wolfgang Windholtz <wwindholtz@me.com>
2016-08-14 21:04:55 -0400

    allow uploading files to the phone

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Summer_robotics.java
@@ -0,0 +1,142 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by wolfie on 7/23/16.
+ */
+
+public class Summer_robotics extends OpMode {
+
+
+    DcMotor Motor1;
+    DcMotor Motor2;
+    DcMotor Motor3;
+    DcMotor Motor4;
+
+    /**
+     * Constructor
+     */
+    public Summer_robotics() {
+
+    }
+
+    /*
+     * Code to run when the op mode is first enabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+     */
+    @Override
+    public void init() {
+		/*
+		 * Use the hardwareMap to get the dc motors and servos by name. Note
+		 * that the names of the devices must match the names used when you
+		 * configured your robot and created the configuration file.
+		 */
+
+		/*
+		 * For the demo Tetrix K9 bot we assume the following,
+		 *   There are two motors "motor_1" and "motor_2"
+		 *   "motor_1" is on the right side of the bot.
+		 *   "motor_2" is on the left side of the bot.
+		 *
+		 * We also assume that there are two servos "servo_1" and "servo_6"
+		 *    "servo_1" controls the arm joint of the manipulator.
+		 *    "servo_6" controls the claw joint of the manipulator.
+		 */
+        Motor1 = hardwareMap.dcMotor.get("Motor1");
+        Motor2 = hardwareMap.dcMotor.get("Motor2");
+        Motor3 = hardwareMap.dcMotor.get("Motor3");
+        Motor4 = hardwareMap.dcMotor.get("Motor4");
+
+        Motor1.setDirection(DcMotor.Direction.REVERSE);
+        Motor2.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+     */
+    @Override
+    public void loop() {
+
+		/*
+		 * Gamepad 1
+		 *
+		 * Gamepad 1 controls the motors via the left stick, and it controls the
+		 * wrist/claw via the a,b, x, y buttons
+		 */
+
+        // tank drive
+        // note that if y equal -1 then joystick is pushed all of the way forward.
+        double left = -gamepad1.left_stick_y;
+        double right = -gamepad1.right_stick_y;
+
+
+        // clip the right/left values so that the values never exceed +/- 1
+        right = Range.clip(right, -1, 1);
+        left = Range.clip(left, -1, 1);
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+        right = (float) scaleInput(right);
+        left = (float) scaleInput(left);
+
+        // write the values to the motors
+        Motor1.setPower(right);
+        Motor2.setPower(right);
+        Motor3.setPower(left);
+        Motor4.setPower(left);
+
+
+		/*
+		 * Send telemetry data back to driver station. Note that if we are using
+		 * a legacy NXT-compatible motor controller, then the getPower() method
+		 * will return a null value. The legacy NXT-compatible motor controllers
+		 * are currently write only.
+		 */
+
+
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm}robotcore.eventloop.opmode.OpMode#stop()
+     */
+
+
+    }
+
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
+}
